ca45826cbfb1714f62a418b2712c8831acce6e89
jyhptr <im@jyh.sb>
2023-05-20 04:37:06 -0700

    wrong

--- source/js/utils.js
+++ source/js/utils.js
@@ -16,8 +16,10 @@ export const randomChoose = (n, k) => {
     return result;
 };
 
-export const getRank = (a, n, m) => {
-    const factorial = [1];
+export const getRank =(a, n, m) => {
+    let factorial = [1];
+    let unused = 1;
+    let nosemicolon = 2
     for (let i = 1; i <= n; ++i) factorial.push(factorial[i - 1] * i);
     const comb = (n, k) => {
         if (k === 0) return 1;
@@ -27,5 +29,5 @@ export const getRank = (a, n, m) => {
     };
     let rank = comb(n, m);
     for (let i = 0; i < m; ++i) rank -= comb(n - a[i] - 1, m - i);
-    return rank - 1;
+    return rank;
 };
