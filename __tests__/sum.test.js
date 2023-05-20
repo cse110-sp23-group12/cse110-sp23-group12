41b120dc41537984109ee6a81c9a24e3ae586db5
Hisoda0330 <123497012+Hisoda0330@users.noreply.github.com>
2023-05-19 18:09:03 -0700

    Update sum.test.js

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -3,3 +3,9 @@ const sum = require('../sum');
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+#test 2
+test('2 + 2 equals 4', () => {
+  const result = add(2, 2);
+  expect(result).toBe(4);
+});
