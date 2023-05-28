791695c99381f996d3c373eafd9103809b39cb18
jyhptr <im@jyh.sb>
2023-05-27 19:19:16 -0700

    done

--- source/config/config.js
+++ source/config/config.js
@@ -7,32 +7,32 @@ const config = {
     selectedLimit: 3,
     cards: [
         {
-            prompt: 'emperor',
+            prompt: 'THE EMPEROR',
             filename: 'tarot_card/emperor.png',
             description: 'The Emperor stands for authority, structure, and paternal influence. It represents leadership and the rational mind, making decisions based on facts and logic.'
         },
         {
-            prompt: 'empress',
+            prompt: 'THE EMPRESS',
             filename: 'tarot_card/empress.png',
             description: 'The Empress symbolizes motherhood, fertility, and abundance. She embodies compassion and unconditional love, nurturing growth and encouraging connections.'
         },
         {
-            prompt: 'fool',
+            prompt: 'THE FOOL',
             filename: 'tarot_card/fool.png',
             description: 'The Fool signifies new beginnings, spontaneity, and a leap of faith. It encourages the embrace of uncertainty and represents the spirit in search of experience.'
         },
         {
-            prompt: 'hierophant',
+            prompt: 'THE HIEROPHANT',
             filename: 'tarot_card/hierophant.png',
             description: 'The Hierophant signifies tradition, convention, and spiritual wisdom. This card suggests seeking spiritual guidance or adhering to established social structures and beliefs.'
         },
         {
-            prompt: 'high priestess',
+            prompt: 'THE HIGH PRIESTNESS',
             filename: 'tarot_card/high_priestess.png',
             description: 'The High Priestess embodies intuition, mystery, and the subconscious mind. She represents the need to trust your instincts and intuition as you proceed through your journey.'
         },
         {
-            prompt: 'magician',
+            prompt: 'THE MAGICIAN',
             filename: 'tarot_card/magician.png',
             description: 'The Magician symbolizes creativity, resourcefulness, and power. It signifies the ability to manifest your goals and desires through determination, focus and understanding the balance of the universe.'
         }
