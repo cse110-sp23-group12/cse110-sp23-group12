801e7ba977a9cbe5ff63cb986ab07686d73bff09
Brandon Szeto <99768694+brandonszeto@users.noreply.github.com>
2023-06-02 18:16:01 -0700

    Added JSDocs comments to main.js

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,21 @@
+/**
+ * Event handler for the window's onload event.
+ */
 window.onload = () => {
     const button = document.getElementById('landing-submit-button');
     const textInput = document.getElementById('text-input');
+    /**
+     * Event handler for the click event on the landing submit button.
+     */
     button.addEventListener('click', () => {
         landingSubmit();
     });
+    /**
+     * Event handler for the keydown event on the text input field.
+     * Triggers the landingSubmit function when the Enter key is pressed.
+     * 
+     * @param {KeyboardEvent} event - The keyboard event object.
+     */
     textInput.addEventListener('keydown', (event) => {
         if (event.keyCode === 13 || event.key === 'Enter') {
             landingSubmit();
@@ -11,6 +23,10 @@ window.onload = () => {
     });
 };
 
+/**
+ * Handles the submission on the landing page.
+ * Redirects to the appropriate page and stores the user's message in localStorage.
+ */
 const landingSubmit = () => {
     const input = document.getElementById('text-input');
     if (input.value === '') return;
