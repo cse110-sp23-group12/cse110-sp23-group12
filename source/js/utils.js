e8e38e103422fe5e82e7b00f9d5104a7f58ef0d5
Brandon Szeto <99768694+brandonszeto@users.noreply.github.com>
2023-06-02 18:24:09 -0700

    JSDocs comments to utils.js

--- source/js/utils.js
+++ source/js/utils.js
@@ -1,8 +1,9 @@
 /**
- * random choose
- * @param {number} n
- * @param {number} k 
- * @returns {Object} 
+ * Randomly chooses k numbers from 0 to n-1 without replacement.
+ * 
+ * @param {number} n - The range of numbers to choose from (0 to n-1).
+ * @param {number} k - The number of numbers to choose.
+ * @returns {number[]} An array of k randomly chosen numbers.
  */
 export const randomChoose = (n, k) => {
     const num = [];
@@ -16,12 +17,26 @@ export const randomChoose = (n, k) => {
     return result;
 };
 
+/**
+ * Calculates the binomial coefficient.
+ * 
+ * @param {number} n - The total number of objects.
+ * @param {number} k - The number of objects to choose.
+ * @returns {number} The binomial coefficient value.
+ */
 export const getRank = (arr, n, m) => {
     const a = [];
     for (let i = 0; i < m; ++i) a.push(arr[i]);
     a.sort((x, y) => { return x - y; });
     const factorial = [1];
     for (let i = 1; i <= n; ++i) factorial.push(factorial[i - 1] * i);
+    /**
+     * Calculates the binomial coefficient.
+     * 
+     * @param {number} n - The total number of objects.
+     * @param {number} k - The number of objects to choose.
+     * @returns {number} The binomial coefficient value.
+     */
     const comb = (n, k) => {
         if (k === 0) return 1;
         if (n === 0 || n < k) return 0;
