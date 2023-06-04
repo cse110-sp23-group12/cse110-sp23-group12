fdfd205210d21810f4a01a785ca430fc2f9fd948
Hisoda0330 <huangpro01@gmail.com>
2023-06-03 22:15:20 -0700

    combine e2e test to one file

--- source/__tests__/home.test.js
+++ source/__tests__/home.test.js
@@ -50,7 +50,22 @@ describe('Basic user flow for Website', () => {
         expect(cookie5).toBeTruthy();
         // cookie6 should not exist so it should be falsy
         expect(cookie6).toBeFalsy();
-    }, 10000)
+    }, 10000);
+
+    it('shows only 6 cookies for select', async () => {
+      const num_cookie = 6;
+      const no_cookie = 6;
+      const limit = 10;
+      for(let i = 0; i < num_cookie; ++i){
+        const cookies = await page.$('#cookie'+ i.toString());
+        expect(cookies).toBeTruthy();
+      }
+      for(let i = no_cookie; i < limit; ++i){
+        const cookies = await page.$('#cookie'+ i.toString());
+        expect(cookies).toBeFalsy();
+      }
+      
+    }, 10000);
 
     it('select 3 cookies', async () => {
       //click 3 cookies
@@ -68,8 +83,9 @@ describe('Basic user flow for Website', () => {
       expect(img1).toBeTruthy();
       expect(img2).toBeTruthy();
       expect(img3).toBeFalsy();
-    }, 10000)
+    }, 10000);
 
+    
     it('check 3 cards are displayed and being added to the plate', async () => {
         const data = await page.evaluate(() => {
             // get all the big-card elements
@@ -77,7 +93,38 @@ describe('Basic user flow for Website', () => {
 
         });
         expect(data.length).toBe(3);
-    }, 10000)
+    }, 10000);
+
+    //checks visibility after selected cookies and correct amount of card in plate
+    it('checks visibility after selected cookies and correct amount of card in plate', async () => {
+      await page.reload();
+      const limit = 6;
+      //
+      let lower = Math.floor(Math.random() * limit);
+      let upper = Math.ceil(Math.random()*(limit - lower) + lower);
+      //make sure no more than 3 to be selected
+      if(upper - lower > 3){
+          upper = Math.ceil(upper/2);
+      }
+      
+      for(let i = lower; i < upper; ++i){
+        //click cookies 
+        await page.evaluate((index) => {
+            document.querySelector('#cookie' + index.toString()).click(); 
+          }, i);
+          const imgselector = '#cookie' + i.toString() + ' .display-none';
+          const img = await page.$(imgselector);
+          //check visibility for selected cookies
+          expect(img).toBeTruthy();
+      }
+      const data = await page.evaluate(() => {
+      // get all the big-card elements
+      return Array.from(document.querySelectorAll("#plate-container .big-card"));
+      });
+      //checks the correct amount in plate
+      expect(data.length).toBe(upper - lower);
+        
+    }, 10000);
 
     it('click button to go to home page', async () => {
         //click button
